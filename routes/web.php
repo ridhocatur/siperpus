91df54db7d78f208562142e9d97919778a6b51ae
ridhocatur <silverhedgie@gmail.com>
2022-04-02 15:36:23 +0800

    Perubahan View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('utama');
+});
+
+Route::get('/daftar', function () {
+    return view('daftar');
 });
