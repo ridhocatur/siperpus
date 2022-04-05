6e15cf10ace49cd467811b46912d2d46e0dceef8
ridhocatur <silverhedgie@gmail.com>
2022-04-06 00:04:45 +0800

    Fix User Model, Rand ID not work

--- app/Http/Controllers/Daftar_c.php
+++ app/Http/Controllers/Daftar_c.php
@@ -3,27 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use app\Models\User;
+use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 class Daftar_c extends Controller
 {
     public function index(){
         return view('daftar', [
-            "title" => "Daftar Akun Baru"
+            'title' => 'Daftar Akun Baru'
         ]);
     }
 
     public function store(Request $regis){
+
+        $randomNumber = random_int(100000, 999999); //masih not work
+
         $validasi = $regis->validate([
             'nama'  => 'max:50',
             'telp'  => 'numeric',
-            'email' => 'required|email|unique:user,email',
+            'email' => 'required|email:dns|unique:user,email',
             'user'  => 'required|min:5|max:25|unique:user,username',
             'pass'  => 'required|min:8'
         ]);
 
         $validasi['pass'] = Hash::make($validasi['pass']);
+        $validasi['id']   = $randomNumber; //masih not work
 
         User::create($validasi);
 
