e4f806a977d6649f01a47ef3dbaf3ebab071d8c4
ridhocatur <silverhedgie@gmail.com>
2022-04-05 15:35:46 +0800

    Perubahan Controller Daftar

--- app/Http/Controllers/Daftar_c.php
+++ app/Http/Controllers/Daftar_c.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class Daftar_c extends Controller
 {
@@ -12,9 +14,19 @@ class Daftar_c extends Controller
         ]);
     }
 
-    public function store(Request $data){
-        $validasi = $data->validate([
-            
+    public function store(Request $regis){
+        $validasi = $regis->validate([
+            'nama'  => 'max:50',
+            'telp'  => 'numeric',
+            'email' => 'required|email|unique:user,email',
+            'user'  => 'required|min:5|max:25|unique:user,username',
+            'pass'  => 'required|min:8'
         ]);
+
+        $validasi['pass'] = Hash::make($validasi['pass']);
+
+        User::create($validasi);
+
+        return view('Anjay MAsuk');
     }
 }
