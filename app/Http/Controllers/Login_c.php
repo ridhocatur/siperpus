50acf3a7d76d44df31305024b59fa829b15ae93d
ridhocatur <silverhedgie@gmail.com>
2022-04-18 04:28:06 +0800

    Belum Fix

--- app/Http/Controllers/Login_c.php
+++ app/Http/Controllers/Login_c.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class Login_c extends Controller
 {
@@ -10,5 +13,23 @@ class Login_c extends Controller
         return view('login', [
             'title' => 'Login | SiPerpus'
         ]);
+    }
+
+    public function masuk(Request $request)
+    {
+        $valid = $request->validate([
+            'user'  => 'required',
+            'pass'  => 'required'
+        ]);
+
+        if (Auth::attempt($valid)) {
+            // $request->session()->regenerate();
+
+            // return redirect()->intended('user_dashboard');
+
+            dd('login berhasil');
+        }
+
+        dd('login gagal',$valid);
     }
 }
